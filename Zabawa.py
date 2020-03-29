36e9baa0036c74c25d76f3caa7429d9f61145e53
Krzysztof-Manys <krzysztof@manys.hub.pl>
2020-03-29 12:42:31 +0200

    First Classes

--- Zabawa.py
+++ Zabawa.py
@@ -1,1 +1,14 @@
-print("OKE")
+﻿# dane
+print("Podaj swoje imie :")
+name = input()
+print("Podaj swoje nazwisko :")
+surname = input()
+print("Podaj swoj wiek :")
+age = input()
+print("Podaj swoja plec :")
+sex = input ()
+print("Wiec nazywasz sie :")
+print(name,surname)
+print("wiek to: ",age)
+print("I jestes",sex)
+
